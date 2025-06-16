e72800a8678cd0eadadc02d66f684f935ad9f4a0
Rafayet Hossen <hossenmdrafayet@gmail.com>
2025-06-16 13:37:05 +0600

    create PasswordGenerator component and add some functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
-import './App.css'
+import React from 'react';
+import PasswordGenerator from './components/PasswordGenerator';
 
-function App() {
-
+const App = () => {
   return (
-    <>
-      <div>
-        <h1>Password Generator</h1>
-        <div className='flex flex-col'>
-            
-            <input type="text" name='password' className='' placeholder='Password'/>
-            <input type="range" name="length" className='' />
-            <label htmlFor="length">Length</label>
-            <input type="checkbox" name="number" />
-            <label htmlFor="number">Number</label>
-            <input type="checkbox" name="symbol" id="" />
-            <label htmlFor="symbol">Symbol</label>
-        </div>
-      </div>
-    </>
-  )
-}
+    <div>
+        
+        <PasswordGenerator/>
+    </div>
+  );
+};
 
-export default App
+export default App;
